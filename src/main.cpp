77b0ec50917929ccb93982b6303f7894b058fdea
Malachi Burke <malachi.burke@gmail.com>
2016-02-26 11:38:14 -0800

    Prepping for customized Adafruit/attiny-friendly sleep code

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,6 @@
 #include <Arduino.h>
+//#include <Adafruit_SleepyDog.h>
+// using code cues from https://arduinoelectronics.wordpress.com/2014/01/06/ultra-low-power-led-flasher-with-attiny/
 
 void setup()
 {

5c34b7758db45851d353b9738a8bb13648d4c2f6
Malachi Burke <malachi.burke@gmail.com>
2016-04-12 00:50:22 -0700

    Cool; sleep mode works

--- src/sleep.cpp
+++ src/sleep.cpp
@@ -1,8 +1,11 @@
 #include <fact/power.h>
+#include <fact/sleep.h>
+#include <fact/lib.h>
 
 #include <avr/sleep.h>
 #include <avr/wdt.h>
 
+using namespace util;
 
 #ifndef cbi
 #define cbi(sfr, bit) (_SFR_BYTE(sfr) &= ~_BV(bit))
@@ -17,6 +20,7 @@
 // system wakes up when wtchdog is timed out
 void system_sleep()
 {
+#ifdef OLD
   cbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter OFF
 
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); // sleep mode is set here
@@ -26,4 +30,11 @@ void system_sleep()
 
   sleep_disable();                     // System continues execution here when watchdog timed out
   sbi(ADCSRA,ADEN);                    // switch Analog to Digitalconverter ON
+#else
+  Power.adc.off();
+  Sleep.powerDown();
+  Power.adc.on();
+
+
+#endif
 }
